d0231134724f86b65f91f2e0db0f84bea5b9b005
Evanka Dsouza <64545413+evankadsouza@users.noreply.github.com>
2024-06-26 17:38:56 +0530

    Update putWalletGameController.js

--- controllers/putWalletGameController.js
+++ controllers/putWalletGameController.js
@@ -10,7 +10,7 @@ const updateWalletAfterGame = {
                 const value =  await connection.query(getQuery);
                 console.log(value)
                 if(value[0][0].value>=0){
-                    const totalPoints = value[0][0].value + (points)
+                    const totalPoints = parseInt(value[0][0].value, 10) + parseInt(points);
                     console.log(value[0][0].value,totalPoints)
                     const updateQuery = `UPDATE walletTable SET value = ${totalPoints}, updatedDateTime =  CONVERT_TZ(NOW(), '+00:00', '+05:30') WHERE userID = ${id}`;
                     const result =  await connection.query(updateQuery);
