97c20509745b7d9342edf60a4feb8026a396bdd9
Evanka Dsouza <64545413+evankadsouza@users.noreply.github.com>
2024-06-28 16:50:30 +0530

    Add files via upload

---
+++ controllers/putActivateUserController.js
@@ -0,0 +1,30 @@
+const connection = require('../config/db');
+
+const updateActivateUser = {
+    updateActivateToggle: async (req, res,next) => {
+        try{
+            const {activate} = req.body;
+            const {id} = req.params;
+            const selectQuery = `SELECT * FROM userTable WHERE id = ${id}`;
+            const selectRes = await connection.query(selectQuery);
+
+            if(selectRes[0][0].id>0)
+                {
+                     const updateQuery = `UPDATE userTable SET activate = ${activate} WHERE id = ${id}`;
+                     const updateRes = await connection.query(updateQuery);
+                     if(updateRes)
+                        {
+                            return res.status(200).json({"Message": `Update of user id ${id} is done with current status as ${activate}`});
+                        }
+                }
+                else{
+                    return res.status(404).json({ error: "Data not found" });
+                }
+        }
+        catch(e){
+            return res.status(500).json({"error":e});
+        }
+    }};
+
+module.exports = updateActivateUser;
+
