c6c9b3fc226c1aaec27ee14353fba94a0505d435
Evanka Dsouza <64545413+evankadsouza@users.noreply.github.com>
2024-07-01 13:55:43 +0530

    Update getCashbackActivitiesController.js

--- controllers/getCashbackActivitiesController.js
+++ controllers/getCashbackActivitiesController.js
@@ -1,15 +1,15 @@
 const connection = require('../config/db');
-
+//controller code to fetch the details from activityCashbackTable, as per the GET request : /getActivityCashback
 const getActivityCashbackDetails = {
     getActivityCashbackData : async (req, res,next) => {
         try{
             const query = `SELECT * FROM activityCashbackTable`;
 
-            const result = await connection.query(query);
+            const result = await connection.query(query);//connection of query to db using sequelize
     
-            if(result)
+            if(result)//if rows returned are more than 0
             {
-                return res.status(200).json({"activityCashbackTable": result[0]})
+                return res.status(200).json({"activityCashbackTable": result[0]})//stores first instance of results in the json which is returned in the response
             }
         }
         catch(error)
