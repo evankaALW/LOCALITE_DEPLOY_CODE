5582f61c9d4ac9d9ee8346a08d18b1fddce2eeee
Evanka Dsouza <64545413+evankadsouza@users.noreply.github.com>
2024-07-01 13:57:11 +0530

    Update getTransactionController.js

--- controllers/getTransactionController.js
+++ controllers/getTransactionController.js
@@ -1,5 +1,5 @@
 const connection = require('../config/db');
-
+//controller code to fetch the details from transactionTable, as per the GET request : /getTransactionDetails
 const getTransactionDetails = {
     transactionData : async (req, res,next) => {
         try{
@@ -12,11 +12,11 @@ const getTransactionDetails = {
             else{
                     query = `SELECT * FROM transactionTable`;
             }
-            const result = await connection.query(query);
+            const result = await connection.query(query);//connection of query to db using sequelize
             
-            if(result)
+            if(result)//if rows returned are more than 0
             {
-                return res.status(200).json({transactionTable: result[0]})
+                return res.status(200).json({transactionTable: result[0]})//stores first instance of results in the json which is returned in the response
             }
         }
         catch(error)
