90bed409e4351026f3a9d0050ce3bd953e805f8b
Evanka Dsouza <64545413+evankadsouza@users.noreply.github.com>
2024-07-01 15:57:51 +0530

    Update postActivityController.js

--- controllers/postActivityController.js
+++ controllers/postActivityController.js
@@ -1,18 +1,18 @@
 const connection = require("../config/db")
-
+//POST request for entering a new cashback activity game details into the activityCashbackTable 
 const postActivityDetails = {
     postActivityData : async (req,res,next) => {
         try{
         var replaced = false;
-
+        //the fields from the body of request (req.body) are stored in the variables below
         const { activityDetails, categoryDetails, cashBackImagesURL, cashbackAmount, dateTime, activityType, categoryType, brandName, gameURL} = req.body;
         
-        const selectQuery = `SELECT id FROM brandTable WHERE brandName='${brandName}'`;
+        const selectQuery = `SELECT id FROM brandTable WHERE brandName='${brandName}'`;//it selects the brandID from the chosen brandName( in the front end dropdown ) to be entered into activityCashbackTable
 
           const brandIDResult = await connection.query(selectQuery);
           const brandID = brandIDResult[0][0].id;
           
-          if(brandID){
+          if(brandID){//if brandID exists executes the INSERT command to enter details into activityCashbackTable
             console.log("BrandID fetched");
 
         const query =  `INSERT INTO activityCashbackTable (activityDetails, categoryDetails, cashBackImagesURL, cashbackAmount, dateAndTime, activityType, categoryType, brandID, createdAt, updatedAt, gameURL) VALUES 
